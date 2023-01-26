1740d250c7e8487730946496818f5dd1434bc08d
yadhukrishnaTM <yadhu3june@gmail.com>
2023-01-26 08:38:43 +0530

    commit

--- lib/Tourisam app ui 2/mainPage.dart
+++ lib/Tourisam app ui 2/mainPage.dart
@@ -19,76 +19,77 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Image(
-            image: NetworkImage(
-                "https://cdn2.iconfinder.com/data/icons/transportation-solid-7/64/transportation-vehicle-21-512.png"),
-            height: 300,
-            width: 300,
-          ),
-          SizedBox(
-            height: 60,
-          ),
-          SizedBox(
-            width: 350,
-            height: 60,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Tourism_first()));
-              },
-              child: const Text("LOGIN",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(100),
-                  )),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Image(
+              image: NetworkImage(
+                  "https://cdn2.iconfinder.com/data/icons/transportation-solid-7/64/transportation-vehicle-21-512.png"),
+              height: 300,
+              width: 300,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: Text("-Or-",style: TextStyle(fontSize: 25,),),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Card(
-            color: CupertinoColors.activeBlue,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(40)),
-            child: ListTile(
-                title: Text("Login With Facebook",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white)),
-                trailing: Image.asset("assets/icons/icons8-facebook-circled-48.png")
+            SizedBox(
+              height: 60,
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Card(
-            color: Colors.blue,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(40)),
-            child: ListTile(
-                title: Text("Login With Twitter",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white)),
-                trailing: Image.asset("assets/icons/icons8-twitter-circled-48.png")
+            SizedBox(
+              width: 350,
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Tourism_first()));
+                },
+                child: const Text("LOGIN",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    )),
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Card(
-            color: Colors.red,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(40)),
-            child: ListTile(
-                title: Text("Login With Google",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white)),
-                trailing: Image.asset("assets/icons/icons8-google-plus-circled-48.png")
+            SizedBox(
+              height: 20,
             ),
-          )
-        ],
+            Center(
+              child: Text("-Or-",style: TextStyle(fontSize: 25,),),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Card(
+              color: CupertinoColors.activeBlue,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40)),
+              child: ListTile(
+                  title: Text("Login With Facebook",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white)),
+                  trailing: Image.asset("assets/icons/icons8-facebook-circled-48.png")
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40)),
+              child: ListTile(
+                  title: Text("Login With Twitter",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white)),
+                  trailing: Image.asset("assets/icons/icons8-twitter-circled-48.png")
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              color: Colors.red,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40)),
+              child: ListTile(
+                  title: Text("Login With Google",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold,color: Colors.white)),
+                  trailing: Image.asset("assets/icons/icons8-google-plus-circled-48.png")),
+            )
+          ],
+        ),
       ),
     );
   }
